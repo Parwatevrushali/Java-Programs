75ff352c11a27570eec99d415cf4d1bb6657eb32
Parwatevrushali <vrushaliparwate01@gmail.com>
2026-01-17 23:11:58 +0530

    Added Java program

--- IncrementDecrementOpeartor/IncrementOperator.java
+++ IncrementDecrementOpeartor/IncrementOperator.java
@@ -7,7 +7,7 @@ class IncrementOperator
 		System.out.println(++poc); //101
 		System.out.println(++poc); //102
 		System.out.println(poc++); //102
-		System.out.println(++poc + poc++); //104+104=208
+		System.out.println(++poc + poc++); //208
 		System.out.println(poc);   //105        
 		System.out.println(poc++); //105
 	}
